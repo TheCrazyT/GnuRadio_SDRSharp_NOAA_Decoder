2bddebefb6d85a069249b73cc25b3382510c740d
CrazyT <TheCrazyT@users.noreply.github.com>
2019-07-22 14:26:32 +0200

    multiprocessing ...

--- find_noa_freq.py
+++ find_noa_freq.py
@@ -16,33 +16,87 @@ from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
+from thread import start_new_thread
+from concurrent.futures import ProcessPoolExecutor,ALL_COMPLETED
 from optparse import OptionParser
 import datetime
+import time
+import concurrent
 import numpy as np
 
 IQ_FILE = r"d:\Program Files (x86)\SDRSharp\NOAA15\SDRSharp_20190112_173333Z_138063526Hz_IQ.wav"
 samp_rate = 2048000
-Center_Frequency = 137622000
-Total_Center_Frequency = 138063526
-Relative_Center_Frequency = Total_Center_Frequency-Center_Frequency
+BW = 819200
+Relative_Center_Frequency = BW/2
 
 NOT_FOUND = True
 FOUND_IN_ROW = 0
-FIR_NEED = 10
+FIR_NEED = 3
 LAST_XFREQ = 0
-THRESHOLD = 2.6
-THRESHOLD2 = 0.1
+THRESHOLD = 1.1
+THRESHOLD2 = 0.5
 THRESHOLD3 = 0.01
 GAP_WIDTH = 1000
-pos = 0
-print("Total_Center_Frequency: %f" % Total_Center_Frequency)
-print("Relative_Center_Frequency: %f" % Relative_Center_Frequency)
+pos = 0.0
 
 
 def upd_samp_rate(sr):
+    global samp_rate
     samp_rate = sr
 
+    
+def find_xfreq(in00,lpos):
+    global samp_rate,THRESHOLD,THRESHOLD2,THRESHOLD3
+    global GAP_WIDTH,NOT_FOUND,FOUND_IN_ROW
+    global LAST_XFREQ
+    global Relative_Center_Frequency,BW
+    
+    rpos = 0
+    X = np.abs(np.fft.fft(in00))
+    if(os.path.isfile("snapshot.txt")):
+        return True
+    print("%s: %06.2f s [%09d] | len(X):%d" % (datetime.datetime.now(),lpos/samp_rate,lpos,len(X)))
+    found = False
+    for i in range(0,len(X)-14*GAP_WIDTH):
+        rpos += 1
+        tcheck1 = True
+        for n in range(0,4):
+            tcheck1 = tcheck1 and (X[i+(n*2)*GAP_WIDTH]>X[i+(n*2+1)*GAP_WIDTH]*THRESHOLD)
+        for n in range(0,4):
+            tcheck1 = tcheck1 and (X[i+(n*2+1)*GAP_WIDTH]*THRESHOLD<X[i+(n*2+2)*GAP_WIDTH])
+        tcheck2 = (abs(X[i]-X[i+2*GAP_WIDTH])<X[i]*THRESHOLD2) and (abs(X[i+2*GAP_WIDTH]-X[i+4*GAP_WIDTH])<X[i+2*GAP_WIDTH]*THRESHOLD2) and (abs(X[i+4*GAP_WIDTH]-X[i+6*GAP_WIDTH])<X[i+4*GAP_WIDTH]*THRESHOLD2)
+        if tcheck1 and tcheck2:
+            found = True
+            xfreq = i*BW/samp_rate-Relative_Center_Frequency
+            if FOUND_IN_ROW != 0:
+                if abs(LAST_XFREQ - xfreq)< (xfreq*THRESHOLD3):
+                    FOUND_IN_ROW = 0
+            LAST_XFREQ = xfreq
+            FOUND_IN_ROW += 1
+            if FOUND_IN_ROW == FIR_NEED:
+                if(os.path.isfile("snapshot.txt")):
+                    return True
+                print("\nFound xfreq: %d at %06.2f s [%09d | i:%d], you can now stop." % (xfreq,lpos/samp_rate,lpos+rpos,i))
+                for n in range(-16,16):
+                    print("%d: %f,%f" % (n,X[i+(n*2)*GAP_WIDTH],X[i+(n*2+1)*GAP_WIDTH]))
+                f = open("snapshot.txt","w+")
+                d = 0
+                for x in X:
+                    f.write("%d,%f\n" % (d,x))
+                    d += 1
+                f.close()
+                NOT_FOUND = False
+                break
+        else:
+            FOUND_IN_ROW = 0
+    print("%s ." % datetime.datetime.now())
+    if not found:
+        FOUND_IN_ROW = 0
+    return not NOT_FOUND
+        
 class find_noa_freq_out(gr.basic_block):
+    executor = ProcessPoolExecutor(max_workers=6)
+    all_futures = []
     def __init__(self):
         global samp_rate ,NOT_FOUND, Relative_Center_Frequency
         gr.basic_block.__init__(
@@ -51,11 +105,16 @@ class find_noa_freq_out(gr.basic_block):
             in_sig = [(np.float32,samp_rate)],
             out_sig = None
         )
+        os.remove("snapshot.txt")
     
+
+
+        
     def general_work(self, input_items, output_items):
         global samp_rate,THRESHOLD,THRESHOLD2,THRESHOLD3
         global GAP_WIDTH,NOT_FOUND,FOUND_IN_ROW
-        global LAST_XFREQ,Relative_Center_Frequency
+        global LAST_XFREQ
+        global Relative_Center_Frequency,BW
         global pos
         
         in0 = input_items[0]
@@ -63,28 +122,26 @@ class find_noa_freq_out(gr.basic_block):
         l = len(in0)
         l00 = len(in00)
         lo = len(output_items)
-        X = np.abs(np.fft.fft(in00))
         
         if NOT_FOUND:
-            print(datetime.datetime.now())
-            found = False
-            for i in range(0,len(X)-7*GAP_WIDTH):
-                pos += 1
-                tcheck1 = (X[i]>X[i+GAP_WIDTH]*THRESHOLD) and (X[i+2*GAP_WIDTH]>X[i+3*GAP_WIDTH]*THRESHOLD) and (X[i+4*GAP_WIDTH]>X[i+5*GAP_WIDTH]*THRESHOLD) and (X[i+6*GAP_WIDTH]>X[i+7*GAP_WIDTH]*THRESHOLD)
-                tcheck2 = (abs(X[i]-X[i+2*GAP_WIDTH])<X[i]*THRESHOLD2) and (abs(X[i+2*GAP_WIDTH]-X[i+4*GAP_WIDTH])<X[i+2*GAP_WIDTH]*THRESHOLD2) and (abs(X[i+4*GAP_WIDTH]-X[i+6*GAP_WIDTH])<X[i+4*GAP_WIDTH]*THRESHOLD2)
-                if tcheck1 and tcheck2:
-                    found = True
-                    xfreq = i*(Total_Center_Frequency-Center_Frequency)/(samp_rate/2)-Relative_Center_Frequency
-                    if FOUND_IN_ROW != 0:
-                        if abs(LAST_XFREQ - xfreq)< (xfreq*THRESHOLD3):
-                            FOUND_IN_ROW = 0
-                    LAST_XFREQ = xfreq
-                    FOUND_IN_ROW += 1
-                    if FOUND_IN_ROW == FIR_NEED:
-                        print("\nFound xfreq: %d at %f s, you can now stop." % (xfreq,pos/samp_rate))
-                        NOT_FOUND = False
-            if not found:
-                FOUND_IN_ROW = 0
+            ndcnt = 0
+            for f in self.all_futures:
+                if not f.done():
+                    ndcnt += 1
+                elif f.result():
+                    NOT_FOUND = False
+                    self.consume(0, l)
+                    self.produce(0, lo)
+                    return 0
+            if(os.path.isfile("snapshot.txt")):
+                return 0
+            if ndcnt>=8:
+                concurrent.futures.wait(self.all_futures,return_when=ALL_COMPLETED)
+            a = self.executor.submit(find_xfreq,in00,pos)
+            self.all_futures.append(a)
+            pos += samp_rate
+            if a is None:
+                raise Exception("FAIL")
         
         self.consume(0, l)
         self.produce(0, lo)
